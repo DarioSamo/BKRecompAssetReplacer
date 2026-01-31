ad1b85da428d501b7a983da3258383e69d0093ba
Dario <dariosamo@gmail.com>
2026-01-30 21:24:02 -0300

    Extend project to asset expansion pak. Rename the mod.

--- src/asset_expansion_pak.c
+++ src/asset_expansion_pak.c
@@ -3,6 +3,7 @@
 #include "functions.h"
 #include "variables.h"
 #include "recomputils.h"
+#include "o1heap.h"
 
 // Forward declarations.
 
@@ -17,26 +18,33 @@ extern u8* assetCacheDependencyCount;
 extern void** assetCachePtrList;
 extern AssetFileMeta *assetSectionRomMetaList;
 extern BKSpriteDisplayData **D_80383CD4;
-extern s32 rarezip_get_uncompressed_size(u8 *arg0);
+extern u32 heap_occupiedBytes;
 
 bool func_8025498C(s32 size);
 void func_80254C98(void);
 void func_8033B020(void *ptr);
 void func_8033BAB0(enum asset_e asset_id, s32 offset, s32 size, void *dst_ptr);
+void func_803449DC(BKSpriteDisplayData *arg0);
 int rarezip_inflate(u8 *src, u8 *dst);
+s32 rarezip_get_uncompressed_size(u8 *arg0);
 
 // Replacement data arrays.
 
 #define MAX_ASSETS 0x4000
+#define ASSET_REPLACEMENT_HEAP_SIZE 0x800000
 
 void *asset_replacements_data[MAX_ASSETS];
 u16 asset_replacements_indices[MAX_ASSETS];
 u16 asset_replacements_active = 0;
+u8 asset_replacement_heap_memory[ASSET_REPLACEMENT_HEAP_SIZE] __attribute__((aligned(0x10)));
+O1HeapInstance *asset_replacement_heap_instance;
 
 RECOMP_HOOK("assetCache_init") void assetCache_init_hook(void) {
     for (u16 i = 0; i < MAX_ASSETS; i++) {
         asset_replacements_indices[i] = MAX_ASSETS;
     }
+
+    asset_replacement_heap_instance = o1heapInit(&asset_replacement_heap_memory[0], ASSET_REPLACEMENT_HEAP_SIZE);
 }
 
 u16 insert_asset_replacement_data(void *asset_data) {
@@ -80,11 +88,11 @@ u16 get_asset_replacement_data_index(void *asset_data) {
     return asset_replacements_data[left] == asset_data ? left : MAX_ASSETS;
 }
 
-RECOMP_EXPORT void bkrecomp_asset_replacer_register(enum asset_e asset_id, void *asset_data) {
+RECOMP_EXPORT void bk_recomp_aep_register_replacement(enum asset_e asset_id, void *asset_data) {
     asset_replacements_indices[asset_id] = insert_asset_replacement_data(asset_data);
 }
 
-RECOMP_EXPORT void bkrecomp_asset_replacer_unregister(enum asset_e asset_id) {
+RECOMP_EXPORT void bk_recomp_aep_unregister_replacement(enum asset_e asset_id) {
     u16 replacement_index = asset_replacements_indices[asset_id];
     if (replacement_index < MAX_ASSETS) {
         asset_replacements_active--;
@@ -97,8 +105,25 @@ RECOMP_EXPORT void bkrecomp_asset_replacer_unregister(enum asset_e asset_id) {
     asset_replacements_indices[asset_id] = MAX_ASSETS;
 }
 
+void *bk_recomp_aep_malloc(s32 size) {
+    return o1heapAllocate(asset_replacement_heap_instance, size);
+}
+
+void bk_recomp_aep_free(void *ptr) {
+    o1heapFree(asset_replacement_heap_instance, ptr);
+}
+
+void *bk_recomp_aep_realloc(void *ptr, s32 size) {
+    void *new_ptr = bk_recomp_aep_malloc(size);
+    memcpy(new_ptr, ptr, size);
+    bk_recomp_aep_free(ptr);
+    return new_ptr;
+}
+
 // @mod Modded to skip asset extraction and return the in-memory replacement if it exists.
 RECOMP_PATCH void *assetcache_get(enum asset_e assetId) {
+    recomp_printf("assetCacheLength %d\n", assetCacheLength);
+
     // @mod If a replacement was made for this asset by another mod, return it instead of extracting it from the game.
     if (asset_replacements_indices[assetId] < MAX_ASSETS) {
         u16 replacement_index = asset_replacements_indices[assetId];
@@ -130,7 +155,9 @@ RECOMP_PATCH void *assetcache_get(enum asset_e assetId) {
         comp_size++;
     sp3C = comp_size;
 
-    if(assetSectionRomMetaList[assetId].compFlag & 0x0001){//compressed
+    // @mod This entire section was rewritten to use the mod's own heap allocation.
+    if (assetSectionRomMetaList[assetId].compFlag & 0x0001) {
+        // Asset is compressed.
         func_8033BAB0(assetId, 0, 0x10, &D_80383CB0);
         assetCacheCurrentSize = rarezip_get_uncompressed_size(&D_80383CB0);
         uncomp_size = assetCacheCurrentSize;
@@ -138,32 +165,28 @@ RECOMP_PATCH void *assetcache_get(enum asset_e assetId) {
             uncomp_size -= uncomp_size & 0xF;
             uncomp_size += 0x10;
         }
-        
-        if (func_8025498C((u32)comp_size + uncomp_size) && !sp28) {
-            sp33 = 1;
-            uncompressed_file = malloc((u32)comp_size + uncomp_size);
-            compressed_file = (void *)((s32) uncompressed_file + uncomp_size);
-        } else {
-            sp33 = 2;
-            if (sp28 != 0) {
-                func_80254C98();
-            }
-            uncompressed_file = malloc(uncomp_size);
-            compressed_file = malloc(comp_size);
+
+        if (sp28 != 0) {
+            func_80254C98();
         }
-    } else { //uncompressed
-        uncompressed_file = malloc(comp_size);
-        compressed_file = uncompressed_file;
-    }
-    piMgr_read(compressed_file, assetSectionRomMetaList[assetId].offset + D_80383CCC, sp3C);
-    if(assetSectionRomMetaList[assetId].compFlag & 0x0001){//decompress
+
+        compressed_file = malloc(comp_size);
+        piMgr_read(compressed_file, assetSectionRomMetaList[assetId].offset + D_80383CCC, sp3C);
+
+        uncompressed_file = bk_recomp_aep_malloc(uncomp_size);
         rarezip_inflate(compressed_file, uncompressed_file);
-        realloc(uncompressed_file, assetCacheCurrentSize);
-        osWritebackDCache(uncompressed_file, assetCacheCurrentSize);
-        if (sp33 == 2) {
-            free(compressed_file);
-        }
+        free(compressed_file);
     }
+    else {
+        // Asset is not compressed. Allocate on the game's heap, read the asset, and then move it to the mod's heap.
+        // piMgr_read will only read correctly to the game's own heap.
+        compressed_file = malloc(comp_size);
+        piMgr_read(compressed_file, assetSectionRomMetaList[assetId].offset + D_80383CCC, sp3C);
+        uncompressed_file = bk_recomp_aep_malloc(comp_size);
+        memcpy(uncompressed_file, compressed_file, comp_size);
+        free(compressed_file);
+    }
+
     assetCacheCurrentIndex = assetCacheLength;
     assetCacheDependencyCount[assetCacheLength] = 1;
     assetCachePtrList[assetCacheLength] = uncompressed_file;
@@ -182,4 +205,67 @@ RECOMP_PATCH void assetCache_free(void *arg0) {
     }
 
     func_8033B020(arg0);
+}
+
+// @mod Patched to replace the malloc routine.
+RECOMP_PATCH void func_8033B2A4(s32 arg0) {
+    assetCachePtrList[assetCacheLength] = bk_recomp_aep_malloc(arg0); // @mod Replaced with the mod's malloc.
+    D_80383CD4[assetCacheLength] = NULL;
+    assetCacheDependencyCount[assetCacheLength] = 1;
+    assetCacheAssetIdList[assetCacheLength] = -1;
+    assetCacheLength += 1;
+}
+
+// @mod Patched to replace the realloc routine.
+RECOMP_PATCH void assetCache_resizeAsset(void *assetPtr, s32 size){
+    s32 tmp;
+    s32 i;
+
+    for(i = 0; i < assetCacheLength  && assetPtr != assetCachePtrList[i]; i++);
+    assetCachePtrList[i] = bk_recomp_aep_realloc(assetPtr, size); // @mod Replaced with the mod's realloc.
+}
+
+// @mod Patched to replace the free routine.
+RECOMP_PATCH s32 assetcache_release(void * arg0){
+    s32 i;
+    if(arg0){
+        for(i = 0; i < assetCacheLength  && arg0 != assetCachePtrList[i]; i++);
+        
+        if(i == assetCacheLength)
+            return 2;
+
+        assetCacheCurrentIndex = i;
+        if(assetCacheDependencyCount[i] == 1){
+            if(D_80383CD4[i])
+                func_803449DC(D_80383CD4[i]);
+            bk_recomp_aep_free(arg0); // @mod Replaced with the mod's free.
+            assetCacheLength--;
+            assetCacheDependencyCount[i] = assetCacheDependencyCount[assetCacheLength];
+            assetCachePtrList[i] = assetCachePtrList[assetCacheLength];
+            D_80383CD4[i] = D_80383CD4[assetCacheLength];
+            assetCacheAssetIdList[i] = assetCacheAssetIdList[assetCacheLength];
+            return 0;
+        }
+        else{
+            assetCacheDependencyCount[i]--;
+            return 1;
+        }
+    } else{
+        return 3;
+    }
+}
+
+// @mod Patched to disable defragmentation for assets entirely.
+RECOMP_PATCH void *defrag_asset(void *arg0){
+    // @mod Running defragmentation for assets is no longer necessary as they don't live on the game's heap.
+    return arg0;
+#if 0
+    void *sp1C;
+    if(arg0 == NULL || arg0 == D_8027659C)
+        return arg0;
+
+    sp1C = defrag(arg0);
+    assetcache_update_ptr(arg0, sp1C);
+    return sp1C;
+#endif
 }
